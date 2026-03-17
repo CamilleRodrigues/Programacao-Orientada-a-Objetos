f82031438dd926442be84a0a0bbe4cf25666c360
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-03-17 20:44:20 -0300

    Create dadosCarro.java

---
+++ AULA 17.03/dadosCarro.java
@@ -0,0 +1,40 @@
+package pkg;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class dadosCarro {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		
+		int n;
+		System.out.println("Digite a quantidade de carro: ");
+		n = teclado.nextInt();
+		
+		teclado.nextLine();
+		
+		Carro[] carro = new Carro[n];
+		
+		for(int i = 0; i < n; i++) {
+			carro[i] = new Carro();
+			System.out.println("Digite a marca do carro " + (i+1) + ": ");
+			carro[i].marca = teclado.nextLine();
+			System.out.println("Digite o modelo do carro " + (i+1) + ": ");
+			carro[i].modelo = teclado.nextLine();
+			System.out.println("Digite o ano de fabricação do carro " + (i+1) + ": ");
+			carro[i].anoFabricacao = teclado.nextInt();
+			teclado.nextLine();
+		}
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println("\nA marca do carro é: " + carro[i].marca);
+			System.out.println("O modelo do carro é: " + carro[i].modelo);
+			System.out.println("O ano de fabricação do carro é: " + carro[i].anoFabricacao);
+		}
+
+  
+	}
+
+}
+
