271ecc195a185567679a75b1e55e757c5d36c00c
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-03-22 16:00:25 -0300

    Create Computador.java

---
+++ AULA 17.03/Exercício Computador (Construtor)/Computador.java
@@ -0,0 +1,16 @@
+package pkg;
+
+public class Carro {
+	public String marca;
+	public String modelo;
+	public String tipo;
+	public float preco;
+	
+	public Carro(String marca, String modelo, String tipo, Float preco) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipo = tipo;
+		this.preco = preco;
+		System.out.println("Marca: " + this.marca + " | Modelo: " + this.modelo + " | Tipo: " + this.tipo + " | Preço: " + this.preco + "\n");
+	}
+}
