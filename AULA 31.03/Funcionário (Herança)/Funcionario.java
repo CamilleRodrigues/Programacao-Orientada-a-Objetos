bff1c095af290846685fde195806311b20ed2a5e
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-04-06 21:02:06 -0300

    Create Funcionario.java

---
+++ AULA 31.03/Funcionário (Herança)/Funcionario.java
@@ -0,0 +1,31 @@
+package pkg;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Gerente g = new Gerente();
+        Vendedor v = new Vendedor();
+
+        System.out.println("Salário do Gerente: R$ " + g.calcularSalario());
+        System.out.println("Salário do Vendedor: R$ " + v.calcularSalario());
+    }
+}
+
+class Funcionario {
+    double calcularSalario() {
+        return 1000.0; 
+    }
+}
+
+class Gerente extends Funcionario {
+    @Override
+    double calcularSalario() {
+        return 5000.0; 
+    }
+}
+
+class Vendedor extends Funcionario {
+    @Override
+    double calcularSalario() {
+        return 2000.0; 
+    }
+}
