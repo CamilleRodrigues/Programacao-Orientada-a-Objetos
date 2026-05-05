9502c7a84b5a981ed8569e6b2f58656a46f86fdb
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-05-04 21:31:52 -0300

    Add Enfermeiro class extending FuncionarioHospital

---
+++ Arquivos Exercícios POO 28-04/Exercício 2/Enfermeiro.java
@@ -0,0 +1,25 @@
+public class Enfermeiro extends FuncionarioHospital {
+    private String setor;
+    private double percentualInsalubridade;
+
+    public Enfermeiro(String nome, String matricula, double salarioBase) {
+        super(nome, matricula, salarioBase);
+    }
+
+    @Override
+    public double calcularSalarioFinal() {
+        double bonusInsalubridade = getSalarioBase() * percentualInsalubridade;
+        double valorHorasExtra = 0;
+        if (getHorasTrabalhadasMensais() > 160) {
+            valorHorasExtra = (getHorasTrabalhadasMensais() - 160) * 50.0; //[cite: 1]
+        }
+        return getSalarioBase() + bonusInsalubridade + valorHorasExtra;
+    }
+
+    public void setPercentualInsalubridade(double percentual) {
+        if (percentual != 0.10 && percentual != 0.20 && percentual != 0.40) {
+            throw new ValorInvalidoException("Percentual de insalubridade não reconhecido pelo sindicato.");
+        }
+        this.percentualInsalubridade = percentual;
+    }
+}
