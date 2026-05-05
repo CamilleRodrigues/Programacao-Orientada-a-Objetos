b95338acb68bd17fc50989224b6855cb374ae51b
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-05-05 20:38:27 -0300

    Add escudoAtivado parameter to NaveCombate constructor

--- Arquivos Exercícios POO 28-04/Exercício 1/NaveCombate.java
+++ Arquivos Exercícios POO 28-04/Exercício 1/NaveCombate.java
@@ -1,24 +1,35 @@
 public class NaveCombate extends NaveEspacial {
-    private int numeroArmas;
+    private int numeroArmas; // Atributos extras
     private boolean escudoAtivado;
 
-    public NaveCombate(String id, double maxComb, double vel, int numeroArmas) {
+    public NaveCombate(String id, double maxComb, double vel, int numeroArmas, boolean escudoAtivado) {
         super(id, maxComb, vel);
         this.numeroArmas = numeroArmas;
+        this.escudoAtivado = escudoAtivado;
     }
 
     @Override
-    public double calcularConsumo(double distancia) {
-        double consumo = distancia * 0.8;
+    public double calcularConsumo(double distancia) { // Se o escudoATivado for true -> entra no IF. Se for false -> retornar a primeira multiplicação
+        double consumo = distancia * 0.8; // Primeira multiplicação
         if (escudoAtivado) {
-            consumo *= 1.15;
+            consumo = consumo * 1.15;
         }
         return consumo;
     }
 
-    public int getNumeroArmas() { return numeroArmas; }
-    public void setNumeroArmas(int numeroArmas) { this.numeroArmas = numeroArmas; }
+    public int getNumeroArmas() { 
+        return numeroArmas; 
+    }
 
-    public boolean isEscudoAtivado() { return escudoAtivado; }
-    public void setEscudoAtivado(boolean escudoAtivado) { this.escudoAtivado = escudoAtivado; }
+    public void setNumeroArmas(int numeroArmas) { 
+        this.numeroArmas = numeroArmas; 
+    }
+
+    public boolean isEscudoAtivado() { 
+        return escudoAtivado; 
+    }
+
+    public void setEscudoAtivado(boolean escudoAtivado) { 
+        this.escudoAtivado = escudoAtivado; 
+    }
 }
