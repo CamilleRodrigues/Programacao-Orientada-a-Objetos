1d76321ab3643ba261f34df5df59e07326fc9274
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-05-05 20:38:58 -0300

    Refactor NaveExploracao class with comments

--- Arquivos Exercícios POO 28-04/Exercício 1/NaveExploracao.java
+++ Arquivos Exercícios POO 28-04/Exercício 1/NaveExploracao.java
@@ -1,20 +1,25 @@
 public class NaveExploracao extends NaveEspacial {
-    private boolean possuiSensoresAvancados;
+    private boolean possuiSensoresAvancados; // Atributo extra
 
     public NaveExploracao(String id, double maxComb, double vel, boolean sensores) {
-        super(id, maxComb, vel);
+        super(id, maxComb, vel); // "Empurrando" para a classe NaveEspacial
         this.possuiSensoresAvancados = sensores;
     }
 
-    @Override
-    public double calcularConsumo(double distancia) {
+    @Override // Classe filha deve implementar o método abstrato da classe mãe (calcularConsumo)
+    public double calcularConsumo(double distancia) { // Boolean = true ou false
         double consumo = distancia * 0.3;
         if (possuiSensoresAvancados) {
-            consumo += 50.0;
+            consumo = consumo + 50.0;
         }
         return consumo;
     }
 
-    public boolean isPossuiSensoresAvancados() { return possuiSensoresAvancados; }
-    public void setPossuiSensoresAvancados(boolean possui) { this.possuiSensoresAvancados = possui; }
+    public boolean isPossuiSensoresAvancados() { 
+        return possuiSensoresAvancados; 
+    }
+
+    public void setPossuiSensoresAvancados(boolean possui) { 
+        this.possuiSensoresAvancados = possui; 
+    }
 }
