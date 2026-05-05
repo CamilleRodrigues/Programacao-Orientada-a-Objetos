9817300d019e17b477035d192e213a11c5de0f6e
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-05-04 21:40:15 -0300

    Refactor constructor and methods for clarity

--- Arquivos Exercícios POO 28-04/Exercício 3/BracoRoboticoMontagem.java
+++ Arquivos Exercícios POO 28-04/Exercício 3/BracoRoboticoMontagem.java
@@ -1,14 +1,18 @@
 public class BracoRoboticoMontagem extends MaquinaIndustrial {
     private double percentualCalibracao = 100.0;
 
-    public BracoRoboticoMontagem(String numeroSerie) { super(numeroSerie); }
+    public BracoRoboticoMontagem(String numeroSerie) { 
+        super(numeroSerie); 
+    }
 
-    public void recalibrar() { this.percentualCalibracao = 100.0; }
+    public void recalibrar() { 
+        this.percentualCalibracao = 100.0; 
+    }
 
     @Override
     public void produzirLote(int quantidade) throws Exception {
         if (!isLigada()) throw new MaquinaDesligadaException("Robô desligado!");
-        if (percentualCalibracao < 20.0) throw new EquipamentoDescalibradoException("Calibração baixa!");[cite: 1]
+        if (percentualCalibracao < 20.0) throw new EquipamentoDescalibradoException("Calibração baixa!");
 
         registrarProducao(quantidade, 15.0); 
         this.percentualCalibracao = Math.max(0, percentualCalibracao - (quantidade * 0.5));
