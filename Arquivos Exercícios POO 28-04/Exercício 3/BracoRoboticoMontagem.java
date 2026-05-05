de2a0856e58061a072b201b6c366e7741cddfc2b
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-05-04 21:39:37 -0300

    Create BracoRoboticoMontagem.java

---
+++ Arquivos Exercícios POO 28-04/Exercício 3/BracoRoboticoMontagem.java
@@ -0,0 +1,17 @@
+public class BracoRoboticoMontagem extends MaquinaIndustrial {
+    private double percentualCalibracao = 100.0;
+
+    public BracoRoboticoMontagem(String numeroSerie) { super(numeroSerie); }
+
+    public void recalibrar() { this.percentualCalibracao = 100.0; }
+
+    @Override
+    public void produzirLote(int quantidade) throws Exception {
+        if (!isLigada()) throw new MaquinaDesligadaException("Robô desligado!");
+        if (percentualCalibracao < 20.0) throw new EquipamentoDescalibradoException("Calibração baixa!");[cite: 1]
+
+        registrarProducao(quantidade, 15.0); 
+        this.percentualCalibracao = Math.max(0, percentualCalibracao - (quantidade * 0.5));
+        System.out.println("Robô " + getNumeroSerie() + " concluiu a montagem.");
+    }
+}
