dc2ddfde0b9829e235b863103c8d26f00afc9462
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-05-05 21:29:27 -0300

    Update AssentoOcupadoException.java

--- Aula 05.05/Exercicio1/AssentoOcupadoException.java
+++ Aula 05.05/Exercicio1/AssentoOcupadoException.java
@@ -1,3 +1,5 @@
+//5. Tratamento de Exceções: Ao tentar reservar um assento, o método deve lançar uma exceção personalizada chamada AssentoOcupadoException caso o assento já esteja reservado. 
+//Se o número do assento passado for menor que 1 ou maior que 100, deve lançar uma IllegalArgumentException.
 public class AssentoOcupadoException extends Exception {
     public AssentoOcupadoException(String mensagem) {
         super(mensagem);
