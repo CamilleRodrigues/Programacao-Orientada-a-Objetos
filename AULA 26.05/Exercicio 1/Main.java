1764584b2913a6cdc891b85e3d1eafb2aaa173e7
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-05-26 20:25:35 -0300

    Update Main.java

--- AULA 26.05/Exercicio 1/Main.java
+++ AULA 26.05/Exercicio 1/Main.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        teclado.useLocale(Locale.US);
+
         List<Produto> carrinho = new ArrayList<>();
 
         System.out.print("Quantos produtos deseja adicionar na lista? ");
@@ -20,34 +23,57 @@ public class Main {
             int tipo = teclado.nextInt();
             teclado.nextLine();
 
-            System.out.print("\nNome do produto: ");
+            System.out.print("Nome do produto: ");
             String nome = teclado.nextLine();
 
-            System.out.print("Preço do produto: ");
-            double preco = teclado.nextDouble();
+            Produto produto = null;
 
-            System.out.print("Avaliação do produto: ");
-            int nota = teclado.nextInt();
+            // While para validar o preço
+            boolean precoValido = false;
 
-            try {
-                Produto produto;
-                if (tipo == 1) {
-                    produto = new ProdutoFisico(nome, preco);
-                } else {
-                    produto = new ProdutoDigital(nome, preco);
+            while (!precoValido) {
+                try {
+                    System.out.print("Preço do produto: ");
+                    double preco = teclado.nextDouble();
+                    teclado.nextLine();
+
+                    if (tipo == 1) {
+                        produto = new ProdutoFisico(nome, preco);
+                    } else {
+                        produto = new ProdutoDigital(nome, preco);
+                    }
+
+                    precoValido = true;
+
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                 }
-                ((Avaliavel) produto).adicionarAvaliacao(nota);
-                carrinho.add(produto);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            } catch (NotaInvalidaException e) {
-                System.out.println(e.getMessage());
             }
+
+            // While para validar a nota de avaliação
+            boolean notaValida = false;
+
+            while (!notaValida) {
+                try {
+                    System.out.print("Avaliação do produto: ");
+                    int nota = teclado.nextInt();
+                    teclado.nextLine();
+
+                    ((Avaliavel) produto).adicionarAvaliacao(nota);
+
+                    notaValida = true;
+
+                } catch (NotaInvalidaException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            carrinho.add(produto);
         }
 
         double total = 0;
 
         for (Produto produto : carrinho) {
+
             double frete = produto.calcularFrete();
             double soma = produto.getPreco() + frete;
 
@@ -57,10 +83,9 @@ public class Main {
             System.out.println("Preço: R$ " + produto.getPreco());
             System.out.println("Frete: R$ " + frete);
             System.out.println("Preço + Frete: R$ " + soma);
-            System.out.println();
         }
 
-        System.out.println("Total da compra = R$ " + total);
+        System.out.println("\nO total da compra foi de R$" + total + " reais");
 
         teclado.close();
     }
