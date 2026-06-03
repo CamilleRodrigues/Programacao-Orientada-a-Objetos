0d471afb8c98cad68d4ab6cf41d2cf911611d1a5
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-06-02 21:27:25 -0300

    Create Avaliavel interface with evaluation methods

---
+++ Trabalho final de P.O.O/Avaliavel.java
@@ -0,0 +1,5 @@
+//Toda classe que implementar Avaliavel será obrigada a possuir esses métodos.
+public interface Avaliavel {
+    void adicionarAvaliacao(AvaliacaoFisica avaliacao);
+    void exibirHistorico();
+}
