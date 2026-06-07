bc262ca9beeeba38c5718ecf1c993716a6442b5a
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-06-07 11:26:45 -0300

    Update GinastaArtistica.java

--- Trabalho final de P.O.O/GinastaArtistica.java
+++ Trabalho final de P.O.O/GinastaArtistica.java
@@ -1,6 +1,7 @@
 //Classe que representa uma atleta da modalidade Ginástica Artística.
 //Herda os atributos e métodos da classe Atleta.
-public class GinastaArtistica extends Atleta implements Avaliavel {
+public class GinastaArtistica extends Atleta {
+
     //Aparelho em que a atleta possui maior destaque.
     private String aparelhoPrincipal;
 
@@ -10,7 +11,7 @@ public class GinastaArtistica extends Atleta implements Avaliavel {
     }
 
     //Construtor que recebe os dados da atleta e da modalidade.
-    public GinastaArtistica(String nome, int idade, double altura, String aparelhoPrincipal) {
+    public GinastaArtistica(String nome, int idade, double altura, String aparelhoPrincipal) throws DadosAtletaInvalidosException {
         super(nome, idade, altura);
         this.aparelhoPrincipal = aparelhoPrincipal;
     }
@@ -24,22 +25,9 @@ public class GinastaArtistica extends Atleta implements Avaliavel {
     }
 
     //Implementação obrigatória do método abstrato da classe Atleta.
+    //Exibe a informação específica da modalidade Ginástica Artística.
     @Override
     public void exibirDadosEspecificos() {
         System.out.println("Aparelho Principal: " + aparelhoPrincipal);
-    }
-
-    @Override
-    public void adicionarAvaliacao(AvaliacaoFisica avaliacao) {
-        historicoAvaliacoes.add(avaliacao);
-    }
-
-    @Override
-    public void exibirHistorico() {
-        //Para cada avaliação presente na lista de avaliações da atleta faça avaliacao.exibirAvaliacao();
-        for (AvaliacaoFisica avaliacao : historicoAvaliacoes) { 
-            avaliacao.exibirAvaliacao();
-            System.out.println("---------------------");
-        }
     }
 }
