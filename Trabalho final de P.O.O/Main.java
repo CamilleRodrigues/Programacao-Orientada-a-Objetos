747db50f0ec96b8501e62a69d7b683a2dccbb81b
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-06-09 20:30:39 -0300

    Update Main.java

--- Trabalho final de P.O.O/Main.java
+++ Trabalho final de P.O.O/Main.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        //Lista que armazenará todas as atletas cadastradas.
         ArrayList<Atleta> atletas = new ArrayList<>();
 
         int opcao = -1;
+
         while (opcao != 0) {
             try {
                 System.out.println("\nSISTEMA DE ACOMPANHAMENTO FÍSICO");
@@ -19,47 +18,154 @@ public class Main {
                 System.out.println("5 - Exibir Histórico de Avaliações");
                 System.out.println("0 - Sair");
 
-                System.out.print("Escolha uma opção: ");
-                opcao = teclado.nextInt();
-                teclado.nextLine();
+                System.out.print("\nEscolha uma opção: ");
+                opcao = Integer.parseInt(teclado.nextLine());
 
                 switch (opcao) {
+
                     case 1:
-                        System.out.print("Nome: ");
-                        String nomeGinasta = teclado.nextLine();
-                        System.out.print("Idade: ");
-                        int idadeGinasta = teclado.nextInt();
-                        System.out.print("Altura: ");
-                        double alturaGinasta = teclado.nextDouble();
-                        teclado.nextLine();
-                        System.out.print("Aparelho Principal: ");
-                        String aparelho = teclado.nextLine();
-                        GinastaArtistica ginasta = new GinastaArtistica(nomeGinasta, idadeGinasta, alturaGinasta, aparelho);
-                        atletas.add(ginasta);
-                        System.out.println("Ginasta cadastrada com sucesso!");
+                        String nomeGinasta;
+                        while (true) {
+                            System.out.print("\nNome: ");
+                            nomeGinasta = teclado.nextLine();
+
+                            if (nomeGinasta.isEmpty()) {
+                                System.out.println("Nome não pode ser vazio.");
+                            } else if (!nomeGinasta.matches("[\\p{L} ]+")) {
+                                System.out.println("Nome não pode conter números.");
+                            } else {
+                                break;
+                            }
+                        }
+
+                        int idadeGinasta;
+                        while (true) {
+                            try {
+                                System.out.print("Idade: ");
+                                idadeGinasta = Integer.parseInt(teclado.nextLine());
+
+                                if (idadeGinasta <= 0) {
+                                    throw new DadosAtletaInvalidosException("Idade deve ser maior que zero.");
+                                }
+                                break;
+
+                            } catch (NumberFormatException e) {
+                                System.out.println("Digite apenas números.");
+                            } catch (DadosAtletaInvalidosException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+
+                        double alturaGinasta;
+                        while (true) {
+                            try {
+                                System.out.print("Altura: ");
+                                alturaGinasta = Double.parseDouble(teclado.nextLine());
+
+                                if (alturaGinasta <= 0) {
+                                    throw new DadosAtletaInvalidosException("Altura deve ser maior que zero.");
+                                }
+                                break;
+
+                            } catch (NumberFormatException e) {
+                                System.out.println("Digite um número válido.");
+                            } catch (DadosAtletaInvalidosException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+
+                        String aparelhoPrincipal;
+                        while (true) {
+                            System.out.print("Aparelho principal: ");
+                            aparelhoPrincipal = teclado.nextLine();
+
+                            if (aparelhoPrincipal.isEmpty()) {
+                                System.out.println("Aparelho não pode ser vazio.");
+                            } else if (!aparelhoPrincipal.matches("[\\p{L} ]+")) {
+                                System.out.println("Aparelho não pode conter números.");
+                            } else {
+                                break;
+                            }
+                        }
+
+                        atletas.add(new GinastaArtistica(nomeGinasta, idadeGinasta, alturaGinasta, aparelhoPrincipal));
+                        System.out.println("Atleta cadastrada!");
                         break;
 
                     case 2:
-                        System.out.print("Nome: ");
-                        String nomeHandebol = teclado.nextLine();
-                        System.out.print("Idade: ");
-                        int idadeHandebol = teclado.nextInt();
-                        System.out.print("Altura: ");
-                        double alturaHandebol = teclado.nextDouble();
-                        teclado.nextLine();
-                        System.out.print("Posição em quadra: ");
-                        String posicao = teclado.nextLine();
-                        Handebolista handebolista = new Handebolista(nomeHandebol, idadeHandebol, alturaHandebol, posicao);
-                        atletas.add(handebolista);
-                        System.out.println("Handebolista cadastrada com sucesso!");
+                        String nomeHandebol;
+                        while (true) {
+                            System.out.print("\nNome: ");
+                            nomeHandebol = teclado.nextLine();
+
+                            if (nomeHandebol.isEmpty()) {
+                                System.out.println("Nome não pode ser vazio.");
+                            } else if (!nomeHandebol.matches("[\\p{L} ]+")) {
+                                System.out.println("Nome não pode conter números.");
+                            } else {
+                                break;
+                            }
+                        }
+
+                        int idadeHandebol;
+                        while (true) {
+                            try {
+                                System.out.print("Idade: ");
+                                idadeHandebol = Integer.parseInt(teclado.nextLine());
+
+                                if (idadeHandebol <= 0) {
+                                    throw new DadosAtletaInvalidosException("Idade deve ser maior que zero.");
+                                }
+                                break;
+
+                            } catch (NumberFormatException e) {
+                                System.out.println("Digite apenas números.");
+                            } catch (DadosAtletaInvalidosException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+
+                        double alturaHandebol;
+                        while (true) {
+                            try {
+                                System.out.print("Altura: ");
+                                alturaHandebol = Double.parseDouble(teclado.nextLine());
+
+                                if (alturaHandebol <= 0) {
+                                    throw new DadosAtletaInvalidosException("Altura deve ser maior que zero.");
+                                }
+                                break;
+
+                            } catch (NumberFormatException e) {
+                                System.out.println("Digite um número válido.");
+                            } catch (DadosAtletaInvalidosException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+
+                        String posicaoEmQuadra;
+                        while (true) {
+                            System.out.print("Posição em quadra: ");
+                            posicaoEmQuadra = teclado.nextLine();
+
+                            if (posicaoEmQuadra.isEmpty()) {
+                                System.out.println("Posição não pode ser vazia.");
+                            } else if (!posicaoEmQuadra.matches("[\\p{L} ]+")) {
+                                System.out.println("Posição não pode conter números.");
+                            } else {
+                                break;
+                            }
+                        }
+                        atletas.add(new Handebolista(nomeHandebol, idadeHandebol, alturaHandebol, posicaoEmQuadra));
+                        System.out.println("Atleta cadastrada!");
                         break;
 
                     case 3:
                         if (atletas.isEmpty()) {
                             System.out.println("Nenhuma atleta cadastrada.");
                         } else {
-                            for (int i = 0; i < atletas.size(); i++) {
-                                System.out.println(i + " - " + atletas.get(i).getNome() + " (" + atletas.get(i).getClass().getSimpleName() +")");
+                            for (Atleta atleta : atletas) {
+                                System.out.println(atleta.getNome() + " (" + atleta.getModalidade() + ")");
                             }
                         }
                         break;
@@ -69,30 +175,81 @@ public class Main {
                             System.out.println("Nenhuma atleta cadastrada.");
                             break;
                         }
+
                         for (int i = 0; i < atletas.size(); i++) {
-                            System.out.println(i + " - " + atletas.get(i).getNome());
+                            System.out.println(i + " - " + atletas.get(i).getNome()
+                                    + " (" + atletas.get(i).getModalidade() + ")");
                         }
 
-                        System.out.print("Escolha a atleta: ");
-                        int indice = teclado.nextInt();
-                        teclado.nextLine();
-                        if(indice < 0 || indice >= atletas.size()) {
+                        System.out.print("\nEscolha a atleta: ");
+                        int indice = Integer.parseInt(teclado.nextLine());
+
+                        if (indice < 0 || indice >= atletas.size()) {
                             System.out.println("Atleta não encontrada.");
                             break;
                         }
-                        System.out.print("Data da avaliação: ");
+
+                        System.out.print("\nData da avaliação: ");
                         String data = teclado.nextLine();
-                        System.out.print("Peso: ");
-                        double peso = teclado.nextDouble();
-                        System.out.print("Percentual de gordura: ");
-                        double gordura = teclado.nextDouble();
-                        System.out.print("Massa muscular: ");
-                        double massa = teclado.nextDouble();
-                        teclado.nextLine();
-                        System.out.print("Observações: ");
-                        String observacoes = teclado.nextLine();
-                        AvaliacaoFisica avaliacao = new AvaliacaoFisica(data, peso, gordura, massa, observacoes);
-                        atletas.get(indice).adicionarAvaliacao(avaliacao);
+
+                        double peso;
+                        while (true) {
+                            try {
+                                System.out.print("Peso: ");
+                                peso = Double.parseDouble(teclado.nextLine());
+
+                                if (peso <= 0) {
+                                    throw new DadosAtletaInvalidosException("Peso deve ser maior que zero.");
+                                }
+                                break;
+
+                            } catch (NumberFormatException e) {
+                                System.out.println("Digite um número válido.");
+                            } catch (DadosAtletaInvalidosException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+
+                        double gordura;
+                        while (true) {
+                            try {
+                                System.out.print("Percentual de gordura: ");
+                                gordura = Double.parseDouble(teclado.nextLine());
+
+                                if (gordura < 0 || gordura > 100) {
+                                    throw new DadosAtletaInvalidosException("Percentual de gordura inválido.");
+                                }
+                                break;
+
+                            } catch (NumberFormatException e) {
+                                System.out.println("Digite um número válido.");
+                            } catch (DadosAtletaInvalidosException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+
+                        double massa;
+                        while (true) {
+                            try {
+                                System.out.print("Massa muscular: ");
+                                massa = Double.parseDouble(teclado.nextLine());
+
+                                if (massa <= 0) {
+                                    throw new DadosAtletaInvalidosException("Massa muscular deve ser maior que zero.");
+                                }
+                                break;
+
+                            } catch (NumberFormatException e) {
+                                System.out.println("Digite um número válido.");
+                            } catch (DadosAtletaInvalidosException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+
+                        atletas.get(indice).adicionarAvaliacao(
+                                new AvaliacaoFisica(data, peso, gordura, massa)
+                        );
+
                         System.out.println("Avaliação cadastrada com sucesso!");
                         break;
 
@@ -101,16 +258,26 @@ public class Main {
                             System.out.println("Nenhuma atleta cadastrada.");
                             break;
                         }
+
                         for (int i = 0; i < atletas.size(); i++) {
-                            System.out.println(i + " - " + atletas.get(i).getNome());
+                            System.out.println(i + " - " + atletas.get(i).getNome()
+                                    + " (" + atletas.get(i).getModalidade() + ")");
                         }
-                        System.out.print("Escolha a atleta: ");
-                        int atletaEscolhida = teclado.nextInt();
-                        if(atletaEscolhida < 0 || atletaEscolhida >= atletas.size()) {
+
+                        System.out.print("\nEscolha a atleta: ");
+                        int atletaEscolhida = Integer.parseInt(teclado.nextLine());
+
+                        if (atletaEscolhida < 0 || atletaEscolhida >= atletas.size()) {
                             System.out.println("Atleta não encontrada.");
                             break;
                         }
-                        atletas.get(atletaEscolhida).exibirHistorico();
+
+                        Atleta atleta = atletas.get(atletaEscolhida);
+
+                        System.out.println("\nAtleta selecionada: "
+                                + atleta.getNome() + " (" + atleta.getModalidade() + ")\n");
+
+                        atleta.exibirHistorico();
                         break;
 
                     case 0:
@@ -121,22 +288,15 @@ public class Main {
                         System.out.println("Opção inválida.");
                 }
 
-            }
-            catch (InputMismatchException e) {
-                System.out.println("Digite apenas números nos campos numéricos.");
-                teclado.nextLine();
-            }
-            catch (DadosAtletaInvalidosException e) {
+            } catch (NumberFormatException e) {
+                System.out.println("Digite valores válidos.");
+            } catch (DadosAtletaInvalidosException e) {
                 System.out.println(e.getMessage());
+            }
 
-            }
-            catch (IndexOutOfBoundsException e) {
-                System.out.println("Atleta não encontrada.");
-            }
-            finally {
-                System.out.println();
-            }
+            System.out.println();
         }
+
         teclado.close();
     }
 }
