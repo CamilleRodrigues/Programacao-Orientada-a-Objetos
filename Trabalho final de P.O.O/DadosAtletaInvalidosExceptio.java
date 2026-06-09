d07dd2a00759e7bd5d008fa5adc92dcef7d13dde
Camille da Silva Rodrigues <camille.srodrigues@ufn.edu.br>
2026-06-09 20:29:51 -0300

    Update DadosAtletaInvalidosExceptio.java

--- Trabalho final de P.O.O/DadosAtletaInvalidosExceptio.java
+++ Trabalho final de P.O.O/DadosAtletaInvalidosExceptio.java
@@ -1,6 +1,5 @@
 //Exceção lançada quando algum dado da atleta é inválido.
 public class DadosAtletaInvalidosException extends Exception {
-
     //Construtor que recebe a mensagem de erro.
     public DadosAtletaInvalidosException(String mensagem) {
         super(mensagem);
